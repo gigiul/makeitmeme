80e3412809e4f214d65192e3b9bdc61de5f55b86
Gigiul <luigidalleaste@gmail.com>
2023-04-01 16:00:50 +0200

    Fe: managed the creation of new texts and archiving of their positions to send everything to the backend

--- fe/src/components/Play.js
+++ fe/src/components/Play.js
@@ -7,10 +7,7 @@ const Play = ({ lastJsonMessage, sendJsonMessage, src }) => {
     const [srcMeme, setSrcMeme] = useState(src)
     const [countdown, setCountdown] = useState()
     const [notSend, setNotSend] = useState(true)
-    const [text, setText] = useState("");
-    const [text2, setText2] = useState("");
-    const [pos, setPos] = useState({ x: 0, y: 0 })
-    const [pos2, setPos2] = useState({ x: 0, y: 0 })
+    const [textArray, setTextArray] = useState([{ text: "", x: 0, y: 0 }])
 
     useEffect(() => {
         switch (lastJsonMessage?.type) {
@@ -23,7 +20,7 @@ const Play = ({ lastJsonMessage, sendJsonMessage, src }) => {
             case "COUNTDOWN":
                 setCountdown(lastJsonMessage?.payload);
                 if (lastJsonMessage?.payload === 0 && notSend) {
-                    handleSend("Tempo scaduto mio padre", pos)
+                    handleSend()
                 }
                 break;
             default:
@@ -34,12 +31,8 @@ const Play = ({ lastJsonMessage, sendJsonMessage, src }) => {
         sendJsonMessage({ type: "SKIP_BASE" })
     }
 
-    function handleSend(text, coordinates) {
-        let textObj = {};
-        textObj.text = text;
-        textObj.x = coordinates.x;
-        textObj.y = coordinates.y;
-        sendJsonMessage({ type: "MEME_DONE", payload: { srcMeme, textObj } })
+    function handleSend() {
+        sendJsonMessage({ type: "MEME_DONE", payload: { srcMeme, textArray } })
         setNotSend(false)
     }
 
@@ -49,50 +42,52 @@ const Play = ({ lastJsonMessage, sendJsonMessage, src }) => {
         element.style.width = (25 + element.scrollWidth) + "px";
     }
 
-    function handleDrag(e, data) {
-        const { x, y } = pos;
-        setPos({ x: x + data.deltaX, y: y + data.deltaY });
-        console.log("x", pos.x, "y", pos.y)
+    function handleDrag(e, data, item, i) {
+        item.x = item.x + data.deltaX;
+        item.y = item.y + data.deltaY;
     }
-    function handleDrag2(e, data) {
-        const { x, y } = pos2;
-        setPos2({ x: x + data.deltaX, y: y + data.deltaY });
-        console.log("x", pos.x, "y", pos.y)
+
+    function handleText(e, i) {
+        let textArrayCopy = [...textArray];
+        textArray[i].text = e.target.value;
+        setTextArray(textArrayCopy)
+        textAreaAdjust(e.target)
+    }
+
+    function createNewTextField() {
+        let textArrayCopy = [...textArray];
+        textArrayCopy.push({ text: "", x: 0, y: 0 })
+        setTextArray(textArrayCopy);
     }
 
     return (
         <div className='overflow-hidden h-screen bg-[#373737] '>
             <div className='relative flex flex-col justify-center items-center h-screen'>
-                <Draggable className=' z-20'
-                    defaultPosition={{ x: 0, y: 0 }}
-                    position={pos}
-                    onDrag={handleDrag}>
-                    <div className='absolute top-0 left-0 uppercase drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] text-white md:text-3xl text-xl font-bold  flex justify-center items-center z-20 ' style={{ '-webkit-text-stroke': '1px black' }}>
-                        <textarea placeholder='text' value={text} onInput={(event) => {
-                            setText(event.target.value);
-                            textAreaAdjust(event.target);
-                        }} spellCheck='false' style={{ resize: 'none' }} className={` bg-${text ? 'transparent' : 'black/50'} md:max-w-[600px] max-w-[300px] text-center overflow-hidden`} />
+                {
+                    textArray.map(function (item, i) {
+                        return (
+                            <Draggable key={i} className=' z-20'
+                                positionOffset={{ x: '-50%', y: '-50%' }}
+                                position={item}
+                                onDrag={(e, data) => handleDrag(e, data, item, i)}>
+                                <div className='absolute top-[50%] left-[50%] uppercase drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] text-white md:text-3xl text-xl font-bold  flex justify-center items-center z-20 ' style={{ '-webkit-text-stroke': '1px black' }}>
+                                    <textarea onChange={(e) => handleText(e, i, item)} placeholder='text' value={item.text} spellCheck='false' style={{ resize: 'none' }} className={` bg-${item.text ? 'transparent' : 'black/50'} md:max-w-[600px] max-w-[300px] text-center overflow-hidden`} />
+                                </div>
+                            </Draggable>)
+                    })
+                }
+                <div className='w-[75%] bg-[#3871F6] bg-opacity-20 py-2 rounded-t-lg border-2  border-black z-10'>
+                    <div className='flex items-center justify-between md:mx-16 mx-4'>
+                        <div className=' md:flex hidden  items-center justify-center rounded-lg border-2 border-b-4 bg-[#E9C543] border-black px-8 py-2 cursor-pointer hover:-translate-y-1 transition-transform' onClick={() => createNewTextField()}><span className=' drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] tracking-wide text-white' style={{ '-webkit-text-stroke': '0.2px black' }}>New Text</span></div>
+                        <p className='font-semibold italic text-2xl drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] tracking-wide text-white'>Time left: <span className='animate-ping'>{countdown}</span></p>
                     </div>
-                </Draggable>
-                <Draggable className=' z-20'
-                    defaultPosition={{ x2: 0, y2: 0 }}
-                    position={pos2}
-                    onDrag={handleDrag2}>
-                    <div className='absolute top-0 left-0 uppercase drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] text-white md:text-3xl text-xl font-bold  flex justify-center items-center z-20 ' style={{ '-webkit-text-stroke': '1px black' }}>
-                        <textarea placeholder='text' value={text2} onInput={(event) => {
-                            setText2(event.target.value);
-                            textAreaAdjust(event.target);
-                        }} spellCheck='false' style={{ resize: 'none' }} className={` bg-${text ? 'transparent' : 'black/50'} md:max-w-[600px] max-w-[300px] text-center overflow-hidden`} />
-                    </div>
-                </Draggable>
-                <div className='w-[75%] bg-[#3871F6] bg-opacity-20 flex items-center justify-center py-2 rounded-t-lg border-2  border-black z-10'>
-                    <p className='font-semibold italic text-2xl drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] tracking-wide text-white'>Countdown: <span className='animate-ping'>{countdown}</span></p>
                 </div>
                 <div className='flex flex-col justify-center text-center items-center py-12 w-[75%] bg-gradient-to-b from-[#365CCC] to-[#4AA0ED] rounded-b-lg border-x-2 border-b-4 border-black'>
                     <img src={srcMeme} alt="meme" className='md:w-[800px] md:h-[600px] w-[400px] h-[300px] drop-shadow-2xl rounded-md border-2 border-black border-opacity-30' />
+                    <div className='mt-4 md:hidden rounded-lg border-2 border-b-4 bg-[#E9C543] border-black px-8 py-2 cursor-pointer hover:-translate-y-1 transition-transform' onClick={() => createNewTextField()}><span className=' drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] tracking-wide text-white' style={{ '-webkit-text-stroke': '0.2px black' }}>New Text</span></div>
                     <div className='mt-4 flex gap-4 justify-center w-full'>
                         <div className='flex items-center justify-center rounded-lg border-2 border-b-4 bg-[#E9C543] border-black px-8 py-2 cursor-pointer hover:-translate-y-1 transition-transform' onClick={() => handleSkip()}><span className=' drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] tracking-wide text-white' style={{ '-webkit-text-stroke': '0.2px black' }}>Skip</span></div>
-                        <div className='flex items-center justify-center rounded-lg border-2 border-b-4 bg-[#E9C543] border-black px-8 py-2 cursor-pointer hover:-translate-y-1 transition-transform' onClick={() => handleSend("Prova con coordinate", pos)}><span className=' drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] tracking-wide text-white' style={{ '-webkit-text-stroke': '0.2px black' }}>Send Meme</span></div>
+                        <div className='flex items-center justify-center rounded-lg border-2 border-b-4 bg-[#E9C543] border-black px-8 py-2 cursor-pointer hover:-translate-y-1 transition-transform' onClick={() => handleSend()}><span className=' drop-shadow-[0_2px_1.2px_rgba(0,0,0,90)] tracking-wide text-white' style={{ '-webkit-text-stroke': '0.2px black' }}>Send</span></div>
                     </div>
                 </div>
             </div>
